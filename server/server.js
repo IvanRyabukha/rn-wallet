479462cac2239a8b8dbc267cd0e195e35d5055d2
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-09-23 17:58:41 +0300

    add get method for summary balance info by userId

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.get("/api/transactions/:userId", async (req, res) => {
   try {
     const transactions = await sql`
       SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC
-    `
+    `;
     res.status(200).json(transactions);
   } catch (error) {
     console.log("Error getting the transactions", error);
@@ -78,20 +78,49 @@ app.delete("/api/transactions/:id", async (req, res) => {
     const { id } = req.params;
 
     if (isNaN(parseInt(id))) {
-      return res.status(400).json({ message: "Invalid transaction ID"});
+      return res.status(400).json({ message: "Invalid transaction ID" });
     }
 
     const result = await sql`
       DELETE FROM transactions WHERE id = ${id} RETURNING *
-    `
+    `;
 
     if (result.length === 0) {
-      return res.status(404).json({ message: "Transaction not found" })
+      return res.status(404).json({ message: "Transaction not found" });
     }
 
-    res.status(200).json({message: "Transaction deleted successfully"});
+    res.status(200).json({ message: "Transaction deleted successfully" });
   } catch (error) {
     console.log("Error deleting the transaction", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.get("/api/transactions/summary/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const balanceResult = await sql`
+      SELECT COALESCE(SUM(amount), 0) as balance FROM transactions WHERE user_id = ${userId}
+    `
+
+    const incomeResult = await sql`
+      SELECT COALESCE(SUM(amount), 0) as income FROM transactions
+      WHERE user_id = ${userId} AND amount > 0
+    `
+
+    const expensesResult = await sql`
+      SELECT COALESCE(SUM(amount), 0) as expenses FROM transactions
+      WHERE user_id = ${userId} AND amount < 0
+    `
+
+    res.status(200).json({
+      balance: balanceResult[0].balance,
+      income: incomeResult[0].income,
+      expenses: expensesResult[0].expenses,
+    })
+  } catch (error) {
+    console.log("Error getting the summary", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
