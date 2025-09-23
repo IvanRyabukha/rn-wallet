a25e38be01099d12a631d1ae71c016398437773a
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-09-23 17:05:17 +0300

    add get and delete method for transactions by userId

--- server/server.js
+++ server/server.js
@@ -38,6 +38,19 @@ app.get("/", (req, res) => {
   res.send("Its working");
 });
 
+app.get("/api/transactions/:userId", async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const transactions = await sql`
+      SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC
+    `
+    res.status(200).json(transactions);
+  } catch (error) {
+    console.log("Error getting the transactions", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 app.post("/api/transactions", async (req, res) => {
   // title, amount category user_id
   try {
@@ -51,12 +64,34 @@ app.post("/api/transactions", async (req, res) => {
       INSERT INTO transactions(user_id, title, amount, category)
       VALUES (${user_id}, ${title}, ${amount}, ${category})
       RETURNING * 
-    `
+    `;
     console.log(transaction);
     res.status(201).json(transaction[0]);
-
   } catch (error) {
     console.log("Error creating the transaction", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.delete("/api/transactions/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (isNaN(parseInt(id))) {
+      return res.status(400).json({ message: "Invalid transaction ID"});
+    }
+
+    const result = await sql`
+      DELETE FROM transactions WHERE id = ${id} RETURNING *
+    `
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: "Transaction not found" })
+    }
+
+    res.status(200).json({message: "Transaction deleted successfully"});
+  } catch (error) {
+    console.log("Error deleting the transaction", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
