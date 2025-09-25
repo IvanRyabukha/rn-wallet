a5016823bef0e7aa11094023429276c18f939d29
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-09-25 21:17:16 +0300

    Config eslint, babel, ts. First build app

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,4 +1,46 @@
+const path = require('path');
+
 module.exports = {
   root: true,
-  extends: '@react-native',
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-native/all',
+    'prettier', // если используешь Prettier
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: true,
+    babelOptions: {
+      configFile: path.resolve(__dirname, 'babel.config.js'),
+    },
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-native', '@typescript-eslint'],
+  rules: {
+    // Общие правила
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'react/prop-types': 'off', // мы используем TS вместо PropTypes
+
+    // React Native
+    'react-native/no-inline-styles': 'off',
+    'react-native/no-color-literals': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
--- client/jest.config.js
+++ client/jest.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native' +
+      '|@react-native' +
+      '|@react-navigation' +
+      '|react-navigation' +
+      '|@react-native-community' +
+      '|react-native-gesture-handler' +
+      '|react-native-reanimated' +
+      ')/)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
