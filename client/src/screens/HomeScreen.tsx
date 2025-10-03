3ed9b7f158f6270e562fb69e112d06a6ffcb538f
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-10-03 11:51:00 +0300

    Create useTransaction hook for receiving transactions data

--- client/src/screens/HomeScreen.tsx
+++ client/src/screens/HomeScreen.tsx
@@ -1,10 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useAuth } from 'providers/AuthProvider'
 import { SignOutButton } from '@components/SignOutButton';
+import { useTransactions } from 'hooks/useTransactions';
 
 const HomeScreen = () => {
   const { user } = useAuth();
+  const { transactions, summary, isLoading, loadData, deleteTransaction } = useTransactions(user?.uid);
+
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
+
+  console.log('userId', user?.uid);
+
+  console.log("transactions", transactions);
+  console.log("summary", summary);
+
   return (
     <View>
       <Text>Hello {user?.email}</Text>
